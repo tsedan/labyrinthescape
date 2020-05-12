faa421190d9311d7dc2f53d021f83b86ee27f5b9
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2020-05-11 23:18:54 -0700

    basic collsion

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,70 @@
 let m = new MazeGenerator(10, 10, 0.05, 5);
 m.generate();
-g = m.grid;
+let player;
+let walls;
+let open;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-}
 
-function draw() {
-    background(51);
-    for (let i = 0; i < g.length; i++) {
-        for (let j = 0; j < g[0].length; j++) {
+    walls = new Group();
+    open = new Group();
+    for (let i = 0; i < m.grid.length; i++) {
+        for (let j = 0; j < m.grid[0].length; j++) {
+            let box = createSprite(i * 25 + 25 / 2, j * 25 + 25 / 2, 25, 25);
             let isPower = false;
             for (let k of m.powerLocs)
                 if (j == k[1] && i == k[0])
                     isPower = true;
 
             if (isPower) {
-                fill(0, 0, 255);
+                box.shapeColor = color(0, 0, 255);
+                open.add(box);
             } else if (j == m.start[1] && i == m.start[0]) {
-                fill(0, 255, 0);
+                box.shapeColor = color(0, 255, 0);
+                open.add(box);
             } else if (j == m.end[1] && i == m.end[0]) {
-                fill(255, 0, 0);
+                box.shapeColor = color(255, 0, 0);
+                open.add(box);
             } else {
-                if (g[i][j] == 1) {
-                    fill(0);
+                if (m.grid[i][j] == 1) {
+                    box.shapeColor = color(0);
+                    walls.add(box);
                 } else {
-                    fill(255);
+                    box.shapeColor = color(255);
+                    open.add(box);
                 }
 
             }
-            rect(i * 25, j * 25, 25, 25);
-
         }
+    }
+
+    player = createSprite(m.start[0] * 25 + 15, m.start[1] * 25 + 15, 15, 15);
+}
+
+function draw() {
+    background(51);
+    checkKeyPress();
+    player.collide(walls);
+
+    drawSprites();
+}
+
+function checkKeyPress() {
+    if (keyDown('a')) {
+        player.position.x -= 4;
+
+    }
+    if (keyDown('d')) {
+        player.position.x += 4;
+
+    }
+    if (keyDown('w')) {
+        player.position.y -= 4;
+
+    }
+    if (keyDown('s')) {
+        player.position.y += 4;
+
     }
 }
